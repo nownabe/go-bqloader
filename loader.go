a71cf8aa7596347f4ae82559c598e2557dc6aad2
nownabe <nownabe@gmail.com>
2020-11-03 14:49:14 +0900

    Remove unused field

--- loader.go
+++ loader.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"log"
 	"sync"
-	"time"
 
 	"cloud.google.com/go/bigquery"
 	"cloud.google.com/go/storage"
@@ -22,38 +21,8 @@ type Loader interface {
 
 // Event is an event from Cloud Storage.
 type Event struct {
-	Name string `json:"name"`
-
-	Kind                    string                 `json:"kind"`
-	ID                      string                 `json:"id"`
-	SelfLink                string                 `json:"selfLink"`
-	Bucket                  string                 `json:"bucket"`
-	Generation              string                 `json:"generation"`
-	Metageneration          string                 `json:"metageneration"`
-	ContentType             string                 `json:"contentType"`
-	TimeCreated             time.Time              `json:"timeCreated"`
-	Updated                 time.Time              `json:"updated"`
-	TemporaryHold           bool                   `json:"temporaryHold"`
-	EventBasedHold          bool                   `json:"eventBasedHold"`
-	RetentionExpirationTime time.Time              `json:"retentionExpirationTime"`
-	StorageClass            string                 `json:"storageClass"`
-	TimeStorageClassUpdated time.Time              `json:"timeStorageClassUpdated"`
-	Size                    string                 `json:"size"`
-	MD5Hash                 string                 `json:"md5Hash"`
-	MediaLink               string                 `json:"mediaLink"`
-	ContentEncoding         string                 `json:"contentEncoding"`
-	ContentDisposition      string                 `json:"contentDisposition"`
-	CacheControl            string                 `json:"cacheControl"`
-	Metadata                map[string]interface{} `json:"metadata"`
-	CRC32C                  string                 `json:"crc32c"`
-	ComponentCount          int                    `json:"componentCount"`
-	Etag                    string                 `json:"etag"`
-	CustomerEncryption      struct {
-		EncryptionAlgorithm string `json:"encryptionAlgorithm"`
-		KeySha256           string `json:"keySha256"`
-	} `json:"customerEncryption"`
-	KMSKeyName    string `json:"kmsKeyName"`
-	ResourceState string `json:"resourceState"`
+	Name   string `json:"name"`
+	Bucket string `json:"bucket"`
 }
 
 // New build a new Loader.
