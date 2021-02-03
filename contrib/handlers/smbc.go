d86698d55667099b1baf14abe73dbcca166caf83
nownabe <nownabe@gmail.com>
2021-02-03 19:54:37 +0900

    Add builder to build handlers for SMBC

---
+++ contrib/handlers/smbc.go
@@ -0,0 +1,65 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"time"
+
+	"go.nownabe.dev/bqloader"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/xerrors"
+)
+
+func parseSMBCWareki(w string) (time.Time, error) {
+	if len(w) != 9 {
+		return time.Time{}, xerrors.Errorf("invalid date format: %s", w)
+	}
+
+	var rekiBase int
+	switch w[0] {
+	case 'H':
+		rekiBase = 1988
+	case 'R':
+		rekiBase = 2018
+	default:
+		return time.Time{}, xerrors.Errorf("%s is not supported", w)
+	}
+
+	wareki, err := strconv.Atoi(w[1:3])
+	if err != nil {
+		return time.Time{}, xerrors.Errorf("failed to parse wareki as int: %w", err)
+	}
+
+	return time.Parse("2006.01.02", fmt.Sprintf("%d%s", wareki+rekiBase, w[3:9]))
+}
+
+// SMBCStatement builds a handler for statements for SMBC (三井住友銀行 入出金明細).
+func SMBCStatement(name, pattern string, t Table, n bqloader.Notifier) *bqloader.Handler {
+	projector := func(ctx context.Context, r []string) ([]string, error) {
+		// 0: date (年月日)
+		t, err := parseSMBCWareki(r[0])
+		if err != nil {
+			return nil, xerrors.Errorf("failed to parse date: %w", err)
+		}
+		r[0] = t.Format("2006-01-02")
+
+		return r, nil
+	}
+
+	return &bqloader.Handler{
+		Name:            name,
+		Pattern:         regexp.MustCompile(pattern),
+		SkipLeadingRows: 1,
+
+		Encoding:  japanese.ShiftJIS,
+		Parser:    bqloader.CSVParser(),
+		Projector: projector,
+		Notifier:  n,
+
+		Project: t.Project,
+		Dataset: t.Dataset,
+		Table:   t.Table,
+	}
+}
---
+++ contrib/handlers/smbc_test.go
@@ -0,0 +1,70 @@
+package handlers_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"go.nownabe.dev/bqloader"
+	"go.nownabe.dev/bqloader/contrib/handlers"
+)
+
+func Test_parseSMBCWareki(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		input  string
+		expect time.Time
+		e      bool
+	}{
+		{input: "S64.01.07", expect: time.Time{}, e: true},
+		{input: "H01.01.08", expect: time.Date(1989, 1, 8, 0, 0, 0, 0, time.UTC), e: false},
+		{input: "H31.04.30", expect: time.Date(2019, 4, 30, 0, 0, 0, 0, time.UTC), e: false},
+		{input: "R01.05.01", expect: time.Date(2019, 5, 1, 0, 0, 0, 0, time.UTC), e: false},
+		{input: "R1.5.2", expect: time.Time{}, e: true},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.input, func(t *testing.T) {
+			actual, err := handlers.ParseSMBCWareki(c.input)
+
+			if c.e {
+				if err == nil {
+					t.Errorf("Expected error didn't occur")
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Unexpected error: %v", err)
+				}
+
+				if !actual.Equal(c.expect) {
+					t.Errorf("Expected %s but %s", c.expect, actual)
+				}
+			}
+		})
+	}
+}
+
+func Test_SMBCStatement(t *testing.T) {
+	t.Parallel()
+
+	const csv = "csv/smbc_statement.csv"
+
+	expected := [][]string{
+		{"2019-12-04", "10389", "", "カ)ビユ-カ-ド", "124001"},
+		{"2019-12-21", "", "160000", "振込　スミトモ タロウ", "284001"},
+		{"2019-12-26", "80980", "", "ミツイスミトモカ-ド (カ", "203021"},
+	}
+
+	h, tl := buildTestHandler(t, csv, handlers.SMBCStatement)
+
+	name := "path_to/smbc_statement.csv"
+	e := bqloader.Event{Name: name, Bucket: "bucket"}
+
+	if err := h.Handle(context.Background(), e); err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	assertEqual(t, expected, tl.result)
+}
